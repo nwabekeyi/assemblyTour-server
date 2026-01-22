96c3c5fceeec4c09ffef312985b0d0ab3e8cffec
Nwabekeyi <chidi90simeon@gmail.com>
2026-01-22 16:32:06 +0100

    working

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # your Vite frontend
     "http://127.0.0.1:5173",
+    "https://assembly-tour2.vercel.app"
 ]
 
 # This is crucial for cookies / auth headers
